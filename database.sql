cc658c924f1cd40726a6afd4e3f644d03d2927a8
John Worley <john.aver.worley@gmail.com>
2018-12-18 13:39:08 -0600

    question 2

--- database.sql
+++ database.sql
@@ -3,7 +3,8 @@
 SELECT * FROM "customers"
 JOIN "addresses" ON "customers"."id"="addresses"."customer_id"
 -- 2. Get all orders and their line items (orders, quantity and product).
-
+SELECT * FROM "orders"
+JOIN "line_items" ON "orders"."id"="line_items"."order_id"
 -- 3. Which warehouses have cheetos?
 
 -- 4. Which warehouses have diet pepsi?
